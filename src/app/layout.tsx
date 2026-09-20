606c07868c4aedbdcb8b0877d10b637c0933fdd1
Emma DA SILVA <emmambamarah@gmail.com>
2026-09-21 00:01:41 +0100

    Mount television shader globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import OldTelevisionBackground from "@/components/ui/OldTelevisionBackground";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
