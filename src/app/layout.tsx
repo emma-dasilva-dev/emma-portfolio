38456ce9b70b1ca52da30a3b7d353a790bce3e6b
Emma DA SILVA <emmambamarah@gmail.com>
2026-09-21 10:10:36 +0100

    Mount custom cursor globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import OldTelevisionBackground from "@/components/ui/OldTelevisionBackground";
+import CustomCursor from "@/components/ui/CustomCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <OldTelevisionBackground />
+        <CustomCursor />
         {children}
       </body>
     </html>
